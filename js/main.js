f0871a71ce831a3decc5696a82e3dfc0f3439325
LuckyMan01 <55346306+LuckyMan01@users.noreply.github.com>
2020-03-30 18:33:39 +0300

    добавив адаптив

--- js/main.js
+++ js/main.js
@@ -7,16 +7,22 @@ $(function(){
     fade: true,
     cssEase: 'linear', 
     autoplay: true,
-    autoplaySpeed: 1000, 
+    autoplaySpeed: 3000, 
     asNavFor: '.slider__nav-jpg'
   });
 
   $('.slider__nav-jpg').slick({ 
     slidesToScroll: 1,
+    slidesToShow: 6,
     asNavFor: '.slider__inner',
     variableWidth: true, 
     dots: false,
     arrows: false,
     focusOnSelect: true
+  });
+
+  $('.header__menu-btn').on('click', function(){
+    $('.header__menu-list').slideToggle();
+   
   });
 });
